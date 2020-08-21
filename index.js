55275fec98c43c0b5047c7e832909a0984003bb4
Helena Liliana Vaquera <helenalvp@gmail.com>
2020-08-21 21:47:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var kittens = ["Milo","Otis","Garfield"]; //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(kittens) {
   kittens.push("Ralph");
+  return kittens;
 }
 
 function destructivelyPrependKitten() {
-
+  
 }
